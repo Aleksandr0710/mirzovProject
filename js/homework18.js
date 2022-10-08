6503e7ca48d7a3ea186a1373ce370d63f34f22f6
Aleksandr Mirzov <sashamirzov@gmail.com>
2022-10-08 15:43:40 +0300

    js

---
+++ js/homework18.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// упражнение 1
+
+let x = 100;
+let y = 323;
+
+let result = x + y;
+
+console.log(result);
+
+// упражнение  2
+
+console.log(Math.max(10, -45, 102, 36, 12, 0, -1));
+
+// упражнение 3
+
+let q = 123.3399;
+console.log(Math.floor(q));
+
+let j = 0.111;
+console.log(Math.ceil(j));
+
+let i = 45.333333;
+console.log(Math.floor(i * 10) / 10);
+
+let f = 3;
+console.log(Math.pow(f, 5)); 
+
+let g = "4e14";
+console.log(g);
+
+let h = "1" !== 1;
+console.log(h);
