c7a859f84aecbb15af37f9ce81fb1503a419e793
Aleksandr Mirzov <sashamirzov@gmail.com>
2022-10-16 20:29:52 +0300

    js

--- js/homework18.js
+++ js/homework18.js
@@ -2,11 +2,10 @@
 
 // упражнение 1
 
-let x = 100;
-let y = 323;
+let x = "100px";
+let y = "323px";
 
-let result = x + y;
-
+let result = parseInt(x) + parseInt(y);
 console.log(result);
 
 // упражнение  2
@@ -22,13 +21,20 @@ let j = 0.111;
 console.log(Math.ceil(j));
 
 let i = 45.333333;
-console.log(Math.floor(i * 10) / 10);
+console.log(i.toFixed(1));
 
 let f = 3;
-console.log(Math.pow(f, 5)); 
+console.log(Math.pow(f, 5));
 
 let g = "4e14";
 console.log(g);
 
-let h = "1" !== 1;
+let h = 1 === 1;
 console.log(h);
+
+// упражнение 4\
+
+0.1 + 0.2 === 0.3; // Вернёт false, почему?
+
+// тк 0.1 + 0.2 будет 0.30000000000000004, а не 0.3 и поэтому они не равны.
+// правильно будет использовать округление результата используя метод toFixed
