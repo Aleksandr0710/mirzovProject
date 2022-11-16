1c7e6962cfc4223cf09bf7d04a0bfb506e4c44e7
Aleksandr Mirzov <sashamirzov@gmail.com>
2022-11-16 12:06:25 +0300

    react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,15 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React это наше все, Боже, помоги мне дальше идти по этому не легкому
-          пути
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          путь
-        </a>
-      </header>
-    </div>
+    <section>
+      <Header />
+      <Main />
+      <Footer />
+    </section>
   );
 }
 
